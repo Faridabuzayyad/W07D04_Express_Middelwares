584896edf0a77a55705ea3f9660a891696d82044
Faridabuzayyad <farid.abuzayyad@gmail.com>
2021-05-13 02:35:34 +0300

    Practice 1 & 2 Done

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+//Practice Q1 A
 const routerMidWare = express.Router();
-const users = [];
+const users = ["John", "Mark"];
 
-//Pulse Check Q5 - B
+//Pulse Check Q5 B
 app.use((req,res,next)=>{
     const err = new Error("No users");
     err.status = 500;
     if(users.length === 0){
         next(err);
     }
+    else{
+        next();
+    }
 
-})
+});
 
 //Pulse check Q4 
 app.use(express.json());
@@ -37,19 +41,30 @@ app.use("/users" ,  logMethod);
 
 app.get("/users", (req, res, next) => {
     res.json(users);
-
 });
 
-//Practice
-
-//Q1
-
-
+//Practice Q1 B
 routerMidWare.use("/users" , (req , res , next)=>{
     console.log(users);
     next();
 })
 app.use(routerMidWare);
+
+//Practice Q2
+app.post("/users/create" , (req, res , next)=>{
+    users.push(req.body.name);
+    res.json(`${req.body.name} has been added`)
+})
+
+routerMidWare.use("/users/create" , (req , res , next)=>{
+    if(req.body.name){
+        console.log(req.body.name);
+    }
+    else{
+        console.log("please Enter A name");
+    }
+    next();
+});
 
 //Pulse Check Q5 - A
 app.use((err , req , res , next)=>{

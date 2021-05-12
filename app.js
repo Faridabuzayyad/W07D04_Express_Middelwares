d77395c14b29d4e4b1e833b012e3305e9b18b7f7
Faridabuzayyad <farid.abuzayyad@gmail.com>
2021-05-13 01:44:39 +0300

    Pulse check All Done and organized

--- app.js
+++ app.js
@@ -1,23 +1,67 @@
 const express = require("express");
-
 const app = express();
-const port = 3000;
+const PORT = 3000;
+const routerMidWare = express.Router();
+const users = [];
 
-const users = ["John", "Mark"];
+//Pulse Check Q5 - B
+app.use((req,res,next)=>{
+    const err = new Error("No users");
+    err.status = 500;
+    if(users.length === 0){
+        next(err);
+    }
 
+})
+
+//Pulse check Q4 
+app.use(express.json());
+
+//Pulse check Q1
 const logUsers = (req , res , next) =>{
     console.log(users);
     next();
-}
+};
 
-app.use(logUsers());
+//Pulse check Q2
+app.use(logUsers);
+
+//Pulse check Q3
+const logMethod = (req , res , next) =>{
+    console.log(req.method);
+    next();
+}
+app.use("/users" ,  logMethod);
+
+
 
 app.get("/users", (req, res, next) => {
-  res.json(users);
+    res.json(users);
+
 });
 
+//Practice
+
+//Q1
 
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+routerMidWare.use("/users" , (req , res , next)=>{
+    console.log(users);
+    next();
+})
+app.use(routerMidWare);
+
+//Pulse Check Q5 - A
+app.use((err , req , res , next)=>{
+    res.status(err.status);
+    res.json({
+            error : {
+            status : err.status,
+            message : err.message,
+        }
+})
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
 });

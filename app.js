3251406878976e351328cdb0edc0f02d38910d43
Faridabuzayyad <farid.abuzayyad@gmail.com>
2021-05-12 22:57:12 +0300

    Pulse check 1 - Done

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const app = express();
+const port = 3000;
+
+const users = ["John", "Mark"];
+
+const logUsers = (req , res , next) =>{
+    console.log(users);
+    next();
+}
+
+app.get("/users", (req, res, next) => {
+  res.json(users);
+});
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
